ced759390b0a4d9f9d9bcb784ce84a4b1fcfef43
Doug Schultz <douglasschultz1@gmail.com>
2024-06-29 09:01:01 -0400

    adding up total time

--- app/models/TimeEntry.ts
+++ app/models/TimeEntry.ts
@@ -4,12 +4,14 @@ export default class TimeEntry {
   end: string
   durationHours: number
   durationMinutes: number
+  durationSeconds: number
 
-  constructor(date: Date, start: string, end: string, durationHours: number, durationMinutes: number) {
+  constructor(date: Date, start: string, end: string, durationHours: number, durationMinutes: number, durationSeconds: number) {
     this.date = date;
     this.start = start
     this.end = end
     this.durationHours = durationHours
     this.durationMinutes = durationMinutes
+    this.durationSeconds = durationSeconds
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   const [time, setTime] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
   const [timeEntries, setTimeEntries] = useState(Array<TimeEntry>)
-  const [timeEntry, setTimeEntry] = useState(new TimeEntry(new Date(), "", "", 0,0))
+  const [timeEntry, setTimeEntry] = useState(new TimeEntry(new Date(), "", "", 0,0,0))
+  const [totalHours, setTotalHours] = useState(0)
+  const [totalMinutes, setTotalMinutes] = useState(0)
 
   const hours = Math.floor(time / 360000);
   const minutes = Math.floor((time % 360000) / 6000);
@@ -30,8 +32,22 @@ export default function Home() {
         timeEntry.end = new Date().toLocaleTimeString()
         timeEntry.durationHours = hours
         timeEntry.durationMinutes = minutes
+        timeEntry.durationSeconds = seconds
         
-        setTimeEntries([...timeEntries, timeEntry])
+        const currentTimeEntries = [...timeEntries, timeEntry]
+
+        let hrs = timeEntry.durationHours + totalHours
+        let min = timeEntry.durationMinutes + totalMinutes
+
+        if(min > 60) {
+          let prevMin = min
+          min = min % 60
+          hrs = hrs + Math.trunc(prevMin / 60)
+        }
+
+        setTimeEntries(currentTimeEntries)
+        setTotalHours(hrs)
+        setTotalMinutes(min)
       }
     }
     
@@ -76,6 +92,9 @@ export default function Home() {
           }
         </tbody>
       </table>
+      <div>
+        Total: {totalHours} hrs {totalMinutes} min
+      </div>
     </div>
   );
 }
